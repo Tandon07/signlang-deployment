a285eae7d7073a54ac7668cb03dab45d4c9e93b9
tando <tandonsaurabh07@gmail.com>
2023-07-15 03:13:13 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/detect', methods=['POST'])
+def detect():
+    os.system("python yolov5/detect.py --weights best.pt --img 416 --conf 0.5 --source 0")
+    # Add any additional code to process the results if needed
+
+    return "Sign language detection completed!"
+
+
+if __name__ == '__main__':
+    app.run()
